ea19140b78ac5835bb8c7d4319dd1a2d9fb8ccae
TeckTrio <amalpullan4@gmail.com>
2026-05-08 15:39:45 +0530

    Revert "style: increase admin footer margins and home button height for improved layout spacing"
    This reverts commit 51945123d87de4d0d2821d5562614b4e5a67f9e0.

--- modules/admin_control.py
+++ modules/admin_control.py
@@ -218,13 +218,13 @@ class AdminControlPage(QWidget):
         footer = QFrame()
         footer.setObjectName("adminFooter")
         flayout = QHBoxLayout(footer)
-        flayout.setContentsMargins(pw(30), ph(20), pw(30), ph(20))
+        flayout.setContentsMargins(pw(20), ph(16), pw(20), ph(16))
 
         home_btn = QPushButton("🏠  Home")
         home_btn.setObjectName("homeBtn")
         home_btn.setCursor(Qt.PointingHandCursor)
         home_btn.clicked.connect(self.go_home)
-        home_btn.setMinimumHeight(ph(60))
+        home_btn.setMinimumHeight(ph(50))
         flayout.addWidget(home_btn)
 
         root.addWidget(footer)
